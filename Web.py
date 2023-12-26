03af1c3eb9b243152d0873828329c0bc73540609
truongpham999 <50629513+PhamLeTruong@users.noreply.github.com>
2023-12-26 23:27:25 +0700

    Update Web.py

--- Web.py
+++ Web.py
@@ -34,7 +34,7 @@ def Preprocessing(feedback):
 st.markdown(
         f"""
         <style>
-        [data-testid='stAppViewContainer'] {{
+        [data-testid='stAppViewContainer1'] {{
             background-image: url("https://raw.githubusercontent.com/PhamLeTruong/Student_feedback_rating_system/main/background.png");
             background-size: cover;
             background-position: center;
@@ -51,7 +51,7 @@ st.markdown(
 st.markdown(
         f"""
         <style>
-        [data-testid='stAppViewContainer'] {{
+        [data-testid='stAppViewContainer2'] {{
             background-image: url("https://raw.githubusercontent.com/PhamLeTruong/Student_feedback_rating_system/main/emotion.png");
             background-size: auto;
             background-position: center top;
