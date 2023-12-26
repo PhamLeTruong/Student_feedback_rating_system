20d42ffdeee37fe5d7c6e8f3563fd13517bb103a
truongpham999 <50629513+PhamLeTruong@users.noreply.github.com>
2023-12-26 21:38:41 +0700

    Update Web.py

--- Web.py
+++ Web.py
@@ -32,16 +32,20 @@ def Preprocessing(feedback):
     return feedback
 
 st.markdown(
-    f"""
-    <style>
-        body {{
-            background-image: url('https://raw.githubusercontent.com/PhamLeTruong/Student_feedback_rating_system/main/background.jpg');
+        f"""
+        <style>
+        [data-testid='stAppViewContainer'] {{
+            background-image: url("https://raw.githubusercontent.com/PhamLeTruong/Student_feedback_rating_system/main/background.jpg");
             background-size: cover;
+            background-position: left top;
+            background-repeat: no-repeat;
         }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+        [data-testid='stHeader'] {{
+            background-color: rgba(0,0,0,0);
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True)
 
 if 'offensive_words' not in st.session_state:
     with open('vn_offensive_words.txt', encoding='utf8') as f:
