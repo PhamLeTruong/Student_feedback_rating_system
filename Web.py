5f2c47ede58a210be97c53f45cfd6f3f25f6f31d
truongpham999 <50629513+PhamLeTruong@users.noreply.github.com>
2023-12-26 21:46:15 +0700

    Update Web.py

--- Web.py
+++ Web.py
@@ -37,7 +37,7 @@ st.markdown(
         [data-testid='stAppViewContainer'] {{
             background-image: url("https://raw.githubusercontent.com/PhamLeTruong/Student_feedback_rating_system/main/background.jpg");
             #background-size: cover;
-            background-position: center;
+            background-position: center top;
             background-repeat: no-repeat;
         }}
         [data-testid='stHeader'] {{
